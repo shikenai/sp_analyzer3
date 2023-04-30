489c3ec340386423b155d79550aa1b02da4bacd7
shikenai <kakehashi.y2788@gmail.com>
2023-04-30 21:58:11 +0900

    Initial commit

---
+++ front/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="wrapper">
+    wrapper
+    <nav id="header">
+
+    </nav>
+    <div id="main">
+      <a href="api/index">wei</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#wrapper {
+  min-height: 800px;
+  display: grid;
+  grid-template:
+        "header  header header" 60px
+        "main    main   main  " 1fr
+        /150px   1fr;
+  gap: 5px;
+}
+
+#header {
+  grid-area: header;
+  border: 1px solid black;
+  padding: 20px;
+}
+
+#left {
+  grid-area: left;
+  border: 1px solid black;
+  text-align: center;
+}
+
+#main {
+  grid-area: main;
+  border: 1px solid black;
+}
+
+
+</style>
