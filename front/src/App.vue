7a96595992714641b8870134eb6f58b9b3fe3219
shikenai <kakehashi.y2788@gmail.com>
2023-05-05 22:22:30 +0900

    axios.postのテストを行う

--- front/src/App.vue
+++ front/src/App.vue
@@ -13,15 +13,29 @@ const registerTrades = () => {
   })
 }
 const getNewTrades = () => {
-  axios.get('api/get_new_trades.json').then(res => {
-    console.log('get new trades')
-  })
+  const confirmed = confirm("本当に実行しますか？");
+  if (confirmed) {
+    axios.get('api/get_new_trades.json').then(res => {
+      console.log('get new trades')
+    })
+  } else {
+    alert('処理を中断しました。')
+  }
 }
 const analyze = () => {
   axios.get('api/analyze.json').then(res => {
     console.log('analyze')
   })
 }
+window.onload = () => {
+  show()
+}
+const reg_judge=()=>{
+  console.log('judge')
+  target_brand_code.value = document.getElementById('brand_code').innerText
+  console.log(target_brand_code.value)
+}
+const target_brand_code = ref('')
 const zero = ref("")
 const p2 = ref('')
 const p1 = ref('')
@@ -32,6 +46,7 @@ const img = ref('')
 
 const brand_code = ref('')
 const brand_name = ref('')
+const brand_url = ref('')
 const show = () => {
   axios.get('api/show.json').then(res => {
     zero.value = JSON.parse(res.data.zero)
@@ -42,15 +57,15 @@ const show = () => {
     console.log(p2.value)
   })
 }
-const drawing =(url)=>{
+const drawing = (url) => {
   console.log('drawing')
   console.log(url)
   img.value = url
   const index = url.indexOf('【')
-  brand_code.value = url.slice(index+1, index+8)
-  console.log(brand_code.value)
-  brand_name.value = url.slice(index+10, url.indexOf('】'))
-  console.log(brand_name.value)
+  brand_code.value = url.slice(index + 1, index + 8)
+  brand_name.value = url.slice(index + 10, url.indexOf('】'))
+  brand_url.value = 'https://kabutan.jp/stock/news?code=' + brand_code.value.slice(0, 4)
+  console.log(brand_url)
 }
 </script>
 
@@ -70,30 +85,37 @@ const drawing =(url)=>{
       <div id="left">
         <p>p2</p>
         <ul>
-          <li v-for="p in p2"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{p.rsi}} : {{p.brand}}</a></li>
+          <li v-for="p in p2"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{ p.rsi }} : {{ p.brand }}</a>
+          </li>
         </ul>
         <p>p1</p>
         <ul>
-          <li v-for="p in p1"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{p.rsi}} : {{p.brand}}</a></li>
+          <li v-for="p in p1"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{ p.rsi }} : {{ p.brand }}</a>
+          </li>
         </ul>
         <p>m2</p>
         <ul>
-          <li v-for="p in m2"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{p.rsi}} : {{p.brand}}</a></li>
+          <li v-for="p in m2"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{ p.rsi }} : {{ p.brand }}</a>
+          </li>
         </ul>
         <p>m1</p>
         <ul>
-          <li v-for="p in m1"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{p.rsi}} : {{p.brand}}</a></li>
+          <li v-for="p in m1"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{ p.rsi }} : {{ p.brand }}</a>
+          </li>
         </ul>
         <p>zero</p>
         <ul>
-          <li v-for="p in zero"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{p.rsi}} : {{p.brand}}</a></li>
+          <li v-for="p in zero"><a @click.prevent="drawing(p.filename)" :href="p.filename">{{ p.rsi }} :
+            {{ p.brand }}</a></li>
         </ul>
       </div>
-      <div id="main">
-        <img :src="img" alt="" height="550" width="775">
+      <div id="main" v-show="img">
+        <img :src="img" alt="" height="540" width="775">
       </div>
-      <div id="right">
-
+      <div id="right" v-show="brand_url">
+        <h5 style="margin-bottom: 0"><span id="brand_code">{{ brand_code }}</span>　<span>{{ brand_name }}</span></h5>
+        <a :href="brand_url" target="_blank">株探リンク</a><br>
+        <button class="styled" type="button" @click="reg_judge">save</button>
       </div>
     </div>
   </div>
@@ -113,8 +135,6 @@ nav {
 }
 
 nav ul {
-  /*margin: 10px;*/
-  /*padding: 0;*/
   vertical-align: middle;
 }
 
@@ -140,20 +160,59 @@ nav ul li a:hover {
   color: #E7DA66;
 }
 
-#left {
-  width: 165px;
+#contents {
+  display: flex;
+  flex-direction: row;
   height: 550px;
+}
+
+#left {
+  flex: 1;
   border: blueviolet solid 3px;
-  float: left;
   overflow-x: scroll;
   white-space: nowrap;
 }
 
+#left p {
+  margin: 0;
+  padding-left: 5px;
+}
+
+#left ul {
+  margin-top: 0;
+  padding-top: 0;
+}
+
 #main {
-  width: 950px;
-  height: 550px;
+  flex: 2;
   border: #E7DA66 solid 2px;
 }
 
+#right {
+  flex: 1;
+  border: green solid 1px;
+}
+
+.styled {
+    border: 0;
+    line-height: 2.5;
+    padding: 0 20px;
+    font-size: 1rem;
+    text-align: center;
+    color: #fff;
+    text-shadow: 1px 1px 1px #000;
+    border-radius: 10px;
+    background-color: rgba(220, 0, 0, 1);
+    background-image: linear-gradient(to top left, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0));
+    box-shadow: inset 2px 2px 3px rgba(255, 255, 255, 0.6), inset -2px -2px 3px rgba(0, 0, 0, 0.6);
+}
+
+.styled:hover {
+    background-color: rgba(255, 0, 0, 1);
+}
+
+.styled:active {
+    box-shadow: inset -2px -2px 3px rgba(255, 255, 255, 0.6), inset 2px 2px 3px rgba(0, 0, 0, 0.6);
+}
 
 </style>
